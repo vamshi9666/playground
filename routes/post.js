82fe06f33c6dc097d6b9a5c04a1f579188ab489c
vamshi9666 <vamshi9666@riseup.net>
2018-09-03 23:01:03 +0530

    cleaned everything for yo nodeserver tool

--- routes/post.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router();
-
-const postController = require('../controllers/post')
-
-router.post('/',postController.add_post)
-
-module.exports = router;
