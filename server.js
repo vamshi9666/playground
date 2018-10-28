84ae5fb6c3d7717e27fe4d4c364cbe5b9d88ba54
vamshi9666 <vamshi9666@riseup.net>
2018-10-28 11:41:44 +0530

    commit for test repo compression branch

--- server.js
+++ server.js
@@ -1,39 +1,12 @@
-const { createServer } = require('http')
-const { parse  } = require('url')
+const zlib = require('zlib');
 
-// const dotenv = require('dotenv').config()
+const input = 'vamshi krishna ';
 
-// const dev = process.env.NODE_ENV !== 'production'
-const next = require('next')
-const app = next(  )
-const handle= app.getRequestHandler()
-
-const port = 3000;
-
-const var = 'port';
-
-app.prepare()
-	.then(()=> {
-		createServer((req,res)=> {
-			const parsedUrl = parse(req.url, true)
-			const { pathname, query  } = parsedUrl
-			if (pathname === '/') {
-				      app.render(req, res, '/', query)
-
-			} else if (pathname === '/about') {
-				      app.render(req, res, '/about', query)
-
-			} else {
-				      handle(req, res, parsedUrl)
-			}
-		})
-		.listen(port, err=> {
-			if(err){
-				console.log(err)
-			}
-			else{
-				console.log(`Server started and ready on port http://localhost:${port}`)
-			}
-
-		})
-	})
+zlib.deflate(input, (err, buffer) => {
+  if (!err) {
+    console.log(buffer.toString('ascii'));
+  } else {
+    console.log(`error is ${err}`);
+  }
+	//comment coe her e for verison 2 
+});
