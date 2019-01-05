b7e3d61b49191fe503e07725f8fc653aa2f4e621
vamshi9666 <vamshi9666@riseup.net>
2019-01-05 22:37:18 +0530

    partial design and vuex basic steps

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,122 @@
 <template>
   <div id="app">
     <div class="whole-wrapper">
-      <div class="row" v-for="(row, i) in array" :key="i" >
-            <div class="col each-tile" v-for="(ele, j ) in row" :key="j">
-                <p>{{ele}}</p>
-            </div>
+      <div >
+        <div class="game-wrapper" v-if="array.length > 0">
+          <div  v-for="(row, i) in array" :key="i">
+            <div class="each-tile">{{row}}</div>
+          </div>
         </div>
+        <div v-else>
+          <img v-bind:src="require('./../load.gif')" alt=" Setting everything up ....">
+          <h3>Setting everything up ....</h3>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'app',
+  import Vue from 'vue'
+  import Vuex, {
+    mapMutations
+  } from 'vuex'
+  Vue.use(Vuex)
+  const store = new Vuex.Store({
+    state: {
+      array: []
+    },
+    mutations: {
+      async init(state) {
+        try {
+          let newArr = []
+          for (var i = 0; i < 16; i++) {
+            console.log("push ", i);
+            state.array.push(i)
+          }
+          console.log(state)
+        } catch (err) {
+          console.log(err)
+        }
   
-  components: {
-  },
-  data: function () {
-    return {
-      array : [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-      ]
+      }
+    },
+    getters: {
+      see(state) {
+        return state.array;
+      }
+    },
+    actions: {
+      init(context) {
+        context.commit('init')
+      }
     }
-      
-    
+  })
+  
+  export default {
+    name: 'app',
+  
+    components: {},
+    // data: function () {
+    //   return {
+    //     array : this.$
+    //   }
+  
+  
+    // },
+    computed: {
+      array() {
+        return store.state.array;
+      }
+    },
+    created: async function() {
+      await this.init()
+      console.log(store.state.array)
+    },
+    methods: {
+      async init() {
+        let res = await store.dispatch('init');
+        console.log(store.state.array);
+  
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.whole-wrapper{
-  width:60%;
-  height: 60%;
-  margin: auto;
-  border: solid 2px #a27cc5 ;
-}
-.row{
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-}
-.col {
-  display:flex;
-  flex-direction: column;
-  justify-content: space-around
-}
-.each-tile {
-  width:40px;
-  height:40px;
-  margin-top: 16px;
-  margin-bottom: 16px;
-  padding: 24px;
-  border: solid 1px yellow; 
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  .whole-wrapper {
+    display: flex;
+    flex: 1;
+    margin: 25px;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+    min-height: 450px;
+    margin: auto;
+    border: solid 2px #a27cc5;
+  }
+  
+  .game-wrapper {
+    display: grid;
+	  grid-template-columns: 25% 25% 25% 25%;
+    grid-template-rows: 25% 25% 25% 25%;
+  }
+  
+  .each-tile {
+    width: 30px;
+    height: 30px;
+    margin: 10px;
+    padding: 20px;
+    text-align: center;
+    border: solid 1px yellow;
+  }
 </style>
