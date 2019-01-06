e2a84f7d71457a0a053db43b4cace8e39ffa6698
vamshi9666 <vamshi9666@riseup.net>
2019-01-06 21:42:39 +0530

    finally !

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div class="whole-wrapper">
-      <div >
+      <div>
         <div class="game-wrapper" v-if="array.length > 0">
-          <div  v-for="(row, i) in array" :key="i">
+          <div v-for="(row, i) in array" :key="i">
             <div class="each-tile">{{row}}</div>
           </div>
         </div>
@@ -17,79 +17,122 @@
 </template>
 
 <script>
-  import Vue from 'vue'
-  import Vuex, {
-    mapMutations
-  } from 'vuex'
-  Vue.use(Vuex)
-  const store = new Vuex.Store({
-    state: {
-      array: []
-    },
-    mutations: {
-      async init(state) {
-        try {
-          let newArr = []
-          for (var i = 0; i < 16; i++) {
-            state.array.push(i)
-          }
-        } catch (err) {
-          console.log(err)
-        }
-  
-      },
-      
-
-    },
-    getters: {
-      see(state) {
-        return state.array;
-      },
-      async findZeros(state){
-        zeroIndexs = [];
-        for ( var i = 0 ; i < 16; i ++){
-          
-        }
-      }
-    },
-    actions: {
-      init(context) {
-        context.commit('init')
-      }
-    }
-  })
+  // import Vue from 'vue'
+  // import createLogger from 'vuex/dist/logger'
+  // import Vuex, {
+  // } from 'vuex'
+  // Vue.use(Vuex)
+  // const store = new Vuex.Store({
+  // plugins: [createLogger()],
+  // state: {
+  // array: [],
+  // zeroIndexs: []
+  // },
+  // mutations: {
+  // async init(state) {
+  // try {
+  // for (var i = 0; i < 16; i++) {
+  // state.array.push(0)
+  // }
+  // } catch (err) {
+  // eslint-disable-next-line
+  // console.log(err)
+  // 
+  // }
+  // },
+  // async findZeros(state) {
+  // let zeroIndexs = [];
+  // state.array.forEach((val, i) => {
+  // if (val === 0) {
+  // zeroIndexs.push(i)
+  // }
+  // })
+  // state.zeroIndexs = zeroIndexs;
+  // },
+  // async insertNumbers(state) {
+  // const zeroIndexs = state.zeroIndexs;
+  // state.array[zeroIndexs[0]] = 5;
+  // }
+  // 
+  // },
+  // getters: {
+  // see(state) {
+  // return state.array;
+  // },
+  // getZeros(state) {
+  // return state.zeroIndexs;
+  // }
+  // },
+  // actions: {
+  // async init(context) {
+  // context.commit('init')
+  // },
+  // async insert_numbers(context) {
+  // context.commit('insertNumbers')
+  // },
+  // async get_zeros(context) {
+  // context.commit('findZeros')
+  // }
+  // }
+  // })
   
   export default {
     name: 'app',
-  
-    components: {},
-    // data: function () {
-    //   return {
-    //     array : this.$
-    //   }
-  
-  
-    // },
-    computed: {
-      array() {
-        return store.state.array;
+    data: function() {
+      return {
+        array: [],
+        zeroTiles: [],
+        score: 0,
       }
     },
     created: async function() {
-      setTimeout(async()=> {
-         await this.init()
-      console.log(store.state.array)
-      }, 4000)
-     
+      console.log(`created`);
+      
+      //  setTimeout(async() => {
+      await this.init()
+      // }, 4000)
+    },
+    mounted: async function() {
+      console.log(`mounted `)
+      await this.getZeros();
+      await this.insertNumbers();
     },
     methods: {
       async init() {
-        let res = await store.dispatch('init');
-        console.log(store.state.array);
-  
+        try {
+          let newArr = []
+          for (var i = 0; i < 16; i++) {
+            newArr.push(0)
+          }
+          this.array = newArr;
+        } catch (err) {
+          // eslint-disable-next-line
+          console.log(err)
+        }
+      },
+      async insertNumbers() {
+        const zeroIndexs = this.zeroTiles;
+        let newArr = [...this.array]
+        newArr[zeroIndexs[0]] = 5;
+        this.array = [...newArr]
+      },
+      async getZeros() {
+        
+        try{
+           let zeroIndexs = [];
+          this.array.forEach((val, i) => {
+              zeroIndexs.push(i)
+          })
+          console.log(zeroIndexs);
+          this.zeroTiles = zeroIndexs
+        }
+        catch (err){
+          console.log(err)
+        }
+         
+        }
       }
     }
-  }
 </script>
 
 <style>
@@ -116,7 +159,7 @@
   
   .game-wrapper {
     display: grid;
-	  grid-template-columns: 25% 25% 25% 25%;
+    grid-template-columns: 25% 25% 25% 25%;
     grid-template-rows: 25% 25% 25% 25%;
   }
   
